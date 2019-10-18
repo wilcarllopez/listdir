3b444b3a809fce5090d31c2a2dc4a61051acf5c7
unknown <wilcarllopez@gmail.com>
2019-10-18 11:17:28 +0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import  setup
+
+setup(name='listdir',
+      version='0.1.0',
+      description='Script for producing a zip and csv file of the path directory provided',
+      url='https://github.com/wilcarllopez/listdir/tree/packaging',
+      author='Wilcarl D. Lopez',
+      author_email='wilcarl_lopez@trendmicro.com',
+      license='MIT')
