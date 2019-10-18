bea5c4c0f51c57a2c816a550749dc1f907c81446
unknown <wilcarllopez@gmail.com>
2019-10-18 15:17:02 +0800

    Updated

--- listdir/listdir.py
+++ listdir/listdir.py
@@ -8,11 +8,11 @@ import zipfile as zip
 import zlib
 
 # Start of functions
-def find_path(path, csvfilename, datetime):
+def find_path(path, csvfilename):
     """Finding the directory of the pathname path"""
     os.chdir(path)
     if os.path.exists(path) == True:
-        csv_save(path, csvfilename, datetime)
+        csv_save(path, csvfilename)
     else:
         return "Path directory doesn't exists"
 
@@ -40,12 +40,10 @@ def md5_hash(filepath):
     md5 = hasher.hexdigest()
     return md5
 
-def csv_save(path, csvfilename, datetime):
+def csv_save(path, csvfilename):
     """csv_save function will get the files and subdirectories of the path provided
      by the user. Then it will be save as a csv file, name provided by the user."""
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    finalfilename = f"{csvfilename} {datetime}.csv"
+    finalfilename = f"{csvfilename} {str(timestamp_name())}.csv"
     with open(finalfilename, 'w+', newline='') as csvFile:
         headwriter = csv.DictWriter(csvFile, fieldnames=["Parent Directory", "Filename", "File Size", "MD5", "SHA-1"])
         headwriter.writeheader()
@@ -58,44 +56,29 @@ def csv_save(path, csvfilename, datetime):
                 sha1 = sha1_hash(filepath)
                 row = [str(r), f, size, md5 , sha1 ]
                 writer.writerow(row)
-    zip_save(finalfilename, csvfilename, datetime)
+    zip_save(finalfilename, csvfilename)
 
-def update_ini():
+def timestamp_name():
     """Updates the time and date for .ini file"""
-    config = configparser.RawConfigParser()
-    config.read('config.ini')
     timestr = time.strftime("%Y%m%d-%I%M%S %p")
-    #try:
-    config.add_section('datetime')
-    config.set('datetime', 'timestr', timestr)
-    with open("config.ini", 'w+') as cfgfile:
-       config.write(cfgfile)
-    #except config.NoSectionError:
-    #    # Create non-existent section
-    #    config.add_section('datetime')
-    #    config.set('datetime', 'timestr', timestr)
-    #    with open("config.ini", 'w+') as cfgfile:
-    #        config.write(cfgfile)
+    return timestr
 
-def zip_save(finalfilename,csvfilename,datetime):
+def zip_save(finalfilename,csvfilename):
+    """Save the csv file to zip file"""
     try:
         compression = zip.ZIP_DEFLATED
     except:
         compression = zip.ZIP_STORED
-    """Save the csv file to zip file"""
-    with zip.ZipFile(f'{csvfilename} {datetime}.zip', 'w', compresslevel=None) as zipFile:
+    with zip.ZipFile(f'{csvfilename} {str(timestamp_name())}.zip', 'w', compresslevel=None) as zipFile:
         zipFile.write(finalfilename, compress_type=compression)
 # end of functions
 
 def main():
+    """Main function of the program"""
     # Updates the config file
     config = configparser.ConfigParser()
     dir = os.path.dirname(__file__)
-    config.read(dir+'config.ini')
-    timestr = time.strftime("%Y%m%d-%I%M%S %p")
-    config.set('datetime', 'timestr', timestr)
-    with open("config.ini", 'w+') as cfgfile:
-        config.write(cfgfile)
+    config.read(path + 'config.ini')
     parser = argparse.ArgumentParser()
     parser.add_argument('path', nargs="?", default=config['default']['path'], help='Path directory')
     parser.add_argument('csvfilename', nargs="?", default=config['default']['csvfilename'],
@@ -104,10 +87,8 @@ def main():
     # Setting up variables
     path = args.path
     csvfilename = args.csvfilename
-    datetime = config['datetime']['timestr']
     # Start of the program
-    find_path(path, csvfilename, datetime)
+    find_path(path, csvfilename)
 
 if __name__ == "__main__":
     main()
-
